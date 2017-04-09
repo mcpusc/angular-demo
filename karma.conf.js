3c9afa60be27a24013c1e7eaedcd0ec872114c2d
Forrest Davie <fdavie@gmail.com>
2017-04-09 14:55:02 -0700

    Customize project metadata; add license info (COPYRIGHT FORREST DAVIE 2017 ALL RIGHTS RESERVED)

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,10 @@
 //jshint strict: false
 module.exports = function(config) {
   config.set({
-
-    basePath: './app',
-
+    basePath:   './app',
+    autoWatch:  true,
+    frameworks: ['jasmine'],
+    browsers:   ['Chrome'],
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
@@ -11,24 +12,15 @@ module.exports = function(config) {
       'components/**/*.js',
       'view*/**/*.js'
     ],
-
-    autoWatch: true,
-
-    frameworks: ['jasmine'],
-
-    browsers: ['Chrome'],
-
     plugins: [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
       'karma-jasmine',
       'karma-junit-reporter'
     ],
-
     junitReporter: {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
     }
-
   });
 };
